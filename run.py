dd7479d8e07b74d67174e852ad7075008f03d755
Anna <akozorkova@gmail.com>
2022-04-05 19:12:12 +0000

    Create function get_random_pair() for accessing google sheet, pulling a random pair of country and capital and modifying capital name as a clue

--- run.py
+++ run.py
@@ -15,9 +15,24 @@ SHEET = GSPREAD_CLIENT.open('world_capital_cities')
 
 country_capital = SHEET.worksheet('country_capital')
 
-user_name = input("Hi there! What's your name? ")
+user_name = input("\nHi there! What's your name? ")
 while user_name == '':
-    print("Oops! That's an empty input")
+    print("\nOops! That's an empty input")
     user_name = input("Try again! Type your name here: ")
-print("Nice meeting you, " + user_name.capitalize())
+print("\nNice meeting you, " + user_name.capitalize() + "\n")
+
+print("Let's play the game that checks how well you know the world's capital cities.")
+print("You will have to type in the capital city for the country I randomly choose.")
+print("Let's start")
+
+def get_random_pair():
+    country_capital= SHEET.worksheet('country_capital').get_all_values()
+    random_choice = random.choice(country_capital)
+    capital = random_choice[1]
+    print(capital[0]+"."*(len(capital)-2)+capital[-1])
+
+get_random_pair()
+
+
+
 
